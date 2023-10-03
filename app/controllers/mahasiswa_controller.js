d09d48cb18beb92f284e5a6870d96bcd99b2ac1a
mahesyasn18 <school.mahesyasn18@gmail.com>
2023-10-03 08:26:18 +0700

    feature: crud mahasiswa

--- app/controllers/mahasiswa_controller.js
+++ app/controllers/mahasiswa_controller.js
@@ -2,82 +2,149 @@ const db = require("../models");
 const Mahasiswa = db.mahasiswa;
 
 exports.create = (req, res) => {
-    if (!req.body.nim) {
-      res.status(400).send({
-        message: "nim cannot be empty!"
+  if (!req.body.nim) {
+    res.status(400).send({
+      message: "nim cannot be empty!",
+    });
+    return;
+  } else if (!req.body.nama) {
+    res.status(400).send({
+      message: "nama cannot be empty!",
+    });
+    return;
+  } else if (!req.body.username) {
+    res.status(400).send({
+      message: "username cannot be empty!",
+    });
+    return;
+  } else if (!req.body.password) {
+    res.status(400).send({
+      message: "password cannot be empty!",
+    });
+    return;
+  } else if (!req.body.no_telp) {
+    res.status(400).send({
+      message: "no_telp cannot be empty!",
+    });
+    return;
+  } else if (!req.body.no_telp_orang_tua) {
+    res.status(400).send({
+      message: "no_telp_orang_tua cannot be empty!",
+    });
+    return;
+  } else if (!req.body.prodiId) {
+    res.status(400).send({
+      message: "prodiId cannot be empty!",
+    });
+    return;
+  } else if (!req.body.kelasId) {
+    res.status(400).send({
+      message: "kelasId cannot be empty!",
+    });
+    return;
+  }
+
+  // Create an mahasiswa
+  const mahasiswa = {
+    nim: req.body.nim,
+    nama: req.body.nama,
+    username: req.body.username,
+    password: req.body.password,
+    no_telp: req.body.no_telp,
+    no_telp_orang_tua: req.body.no_telp_orang_tua,
+    roleId: 2,
+    prodiId: req.body.prodiId,
+    kelasId: req.body.kelasId,
+  };
+
+  Mahasiswa.create(mahasiswa)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Mahasiswa.",
       });
-      return;
-    }else if(!req.body.nama){
-        res.status(400).send({
-            message: "nama cannot be empty!"
-          });
-        return;
-    }else if(!req.body.username){
-        res.status(400).send({
-            message: "username cannot be empty!"
-          });
-        return;
-    }else if(!req.body.password){
-        res.status(400).send({
-            message: "password cannot be empty!"
-          });
-        return;
-    }else if(!req.body.no_telp){
-        res.status(400).send({
-            message: "no_telp cannot be empty!"
-          });
-        return;
-    }else if(!req.body.no_telp_orang_tua){
-        res.status(400).send({
-            message: "no_telp_orang_tua cannot be empty!"
-          });
-        return;
-    }else if(!req.body.prodiId){
-        res.status(400).send({
-            message: "prodiId cannot be empty!"
-          });
-        return;
-    }else if(!req.body.kelasId){
-        res.status(400).send({
-            message: "kelasId cannot be empty!"
-          });
-        return;
-    }
-  
-    // Create an mahasiswa
-    const mahasiswa = {
-        nim: req.body.nim,
-        nama: req.body.nama,
-        username: req.body.username,
-        password: req.body.password,
-        no_telp: req.body.no_telp,
-        no_telp_orang_tua: req.body.no_telp_orang_tua,
-        roleId: 2,
-        prodiId: req.body.prodiId,
-        kelasId: req.body.kelasId,
-    };
-  
-    Mahasiswa.create(mahasiswa)
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while creating the Admin."
-        });
-      });
+    });
 };
 
 exports.findAll = (req, res) => {
-    Mahasiswa.findAll()
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while retrieving Kelas."
-        });
+  Mahasiswa.findAll()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving Kelas.",
       });
+    });
+};
+
+exports.update = (req, res) => {
+  const id = req.params.id;
+
+  Mahasiswa.update(req.body, {
+    where: { id: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Mahasiswa was updated successfully.",
+        });
+      } else {
+        res.send({
+          message: `Cannot update Mahasiswa with id=${id}. Maybe Mahasiswa was not found or req.body is empty!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error updating Mahasiswa with id=" + id,
+      });
+    });
+};
+
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Mahasiswa.destroy({
+    where: { nim: id },
+  })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Mahasiswa was deleted successfully!",
+        });
+      } else {
+        res.send({
+          message: `Cannot delete Mahasiswa with id=${id}. Maybe Mahasiswa was not found!`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not Mahasiswa with id=" + id,
+      });
+    });
+};
+
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  Mahasiswa.findByPk(id)
+    .then((data) => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find Mahasiswa with id=${id}.`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error retrieving Mahasiswa with id=" + id,
+      });
+    });
 };
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -4,26 +4,15 @@ const admin = require("../controllers/admin_controller");
 const kelas = require("../controllers/kelas_controller");
 const mahasiswa = require("../controllers/mahasiswa_controller");
 
-module.exports = app => {
-    app.use(function(req, res, next) {
-        res.header(
-          "Access-Control-Allow-Headers",
-          "Origin, Content-Type, Accept"
-        );
-        next();
-      });
+module.exports = (app) => {
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+    next();
+  });
 
-  app.get(
-    "/api/test/user",
-    [authJwt.verifyToken],
-    controller.userBoard
-  );
+  app.get("/api/test/user", [authJwt.verifyToken], controller.userBoard);
 
-  app.get(
-    'api/admins',
-    [authJwt.verifyToken, authJwt.isAdmin],
-    admin.findAll
-  );
+  app.get("api/admins", [authJwt.verifyToken, authJwt.isAdmin], admin.findAll);
 
   app.get(
     "/api/test/admin",
@@ -47,5 +36,23 @@ module.exports = app => {
     "/api/admins/mahasiswa/create",
     [authJwt.verifyToken, authJwt.isAdmin],
     mahasiswa.create
+  );
+
+  app.put(
+    "/api/admins/mahasiswa/update/:nim",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    mahasiswa.update
+  );
+
+  app.delete(
+    "/api/admins/mahasiswa/destroy/:id",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    mahasiswa.delete
+  );
+
+  app.get(
+    "/api/admins/mahasiswa/:id",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    mahasiswa.findOne
   );
 };
