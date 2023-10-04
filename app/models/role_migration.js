86603d959eebebcb9541daaca966111f499efb7c
RizkiGunawan09 <123919208+RizkiGunawan09@users.noreply.github.com>
2023-10-04 09:23:42 +0700

    Update role_migration.js
    edit id menjadi String

--- app/models/role_migration.js
+++ app/models/role_migration.js
@@ -1,7 +1,7 @@
 module.exports = (sequelize, Sequelize) => {
     const Role = sequelize.define("roles", {
       id: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.STRING,
         primaryKey: true
       },
       name: {
