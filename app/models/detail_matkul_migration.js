5059353b91b08e4a19290420a9b0f5d298fe30aa
fikm21 <taufikm211@gmail.com>
2023-10-17 23:13:37 +0700

    fix route dan migrate data mata kuliah

--- app/models/detail_matkul_migration.js
+++ app/models/detail_matkul_migration.js
@@ -2,8 +2,9 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize, Sequelize) => {
   const detailMatkul = sequelize.define("detailMatkul", {
     id_detailMatkul: {
-      type: DataTypes.STRING,
+      type: DataTypes.INTEGER,
       primaryKey: true,
+      autoIncrement: true,
     },
     tipe: {
       type: DataTypes.STRING,
