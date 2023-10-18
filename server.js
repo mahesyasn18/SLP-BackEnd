daa8aed004fd2652052c44b32635f7a4a6f3e924
Rizki Gunawan <rizki.gunawan.tif22@polban.ac.id>
2023-10-18 07:08:33 +0700

    jadwal controller dan seeders nya

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const cookieSession = require("cookie-session");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const cookieSession = require('cookie-session');
 
 const app = express();
 
 //for react
 var corsOptions = {
-  origin: "http://localhost:3000",
+  origin: 'http://localhost:3000',
   credentials: true,
 };
 
@@ -16,17 +16,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cookieSession({
-    name: "slp-session",
-    keys: ["COOKIE_SECRET"],
+    name: 'slp-session',
+    keys: ['COOKIE_SECRET'],
     httpOnly: true,
   })
 );
 
 //for db
-const db = require("./app/models");
-const seeders = require("./app/seeders");
+const db = require('./app/models');
+const seeders = require('./app/seeders');
 // db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and Resync Db");
+//   console.log('Drop and Resync Db');
 //   seeders.initialRole();
 //   seeders.initialAdmin();
 //   seeders.initialKelas();
@@ -42,12 +42,12 @@ const seeders = require("./app/seeders");
 // });
 // db.sequelize.sync({ force: true });
 db.sequelize.sync();
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome To SLP apps" });
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome To SLP apps' });
 });
 
-require("./app/routes/auth.routes")(app);
-require("./app/routes/routes")(app);
+require('./app/routes/auth.routes')(app);
+require('./app/routes/routes')(app);
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
