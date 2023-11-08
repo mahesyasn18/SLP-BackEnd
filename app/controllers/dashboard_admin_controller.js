0846179316892fdbe8b0567cdd8ef9065734fd3e
adrianeka <78060253+adrianeka@users.noreply.github.com>
2023-11-08 11:33:49 +0700

    update dashboard

--- app/controllers/dashboard_admin_controller.js
+++ app/controllers/dashboard_admin_controller.js
@@ -1,12 +1,14 @@
-const db = require('../models');
+const db = require("../models");
 const Perizinan = db.perizinan;
 const Semester = db.semester;
 
 exports.findAll = (req, res) => {
   const userId = req.userId;
-  Perizinan.findAll({
+
+  // Membuat Promise untuk mengambil data Perizinan
+  const fetchPerizinan = Perizinan.findAll({
     where: {
-      status: 'Diverifikasi',
+      status: "Diverifikasi",
     },
     include: [
       {
@@ -16,21 +18,43 @@ exports.findAll = (req, res) => {
         },
       },
     ],
-  })
-    .then((data) => {
-      const dashboardData = { jumlahSakit: 0, jumlahIzin: 0 };
-      data.forEach((item) => {
-        if (item.jenis === 'Sakit') {
+  });
+
+  // Membuat Promise untuk mengambil data Semester dengan status_semester: 1
+  const fetchActiveSemester = Semester.findAll({
+    where: { status_semester: 1 },
+  });
+
+  // Menjalankan kedua Promise tersebut secara bersamaan
+  Promise.all([fetchPerizinan, fetchActiveSemester])
+    .then((results) => {
+      const perizinanData = results[0];
+      const semesterData = results[1];
+
+      const dashboardData = {
+        jumlahSakit: 0,
+        jumlahIzin: 0,
+        tahun_akademik: null,
+      };
+
+      perizinanData.forEach((item) => {
+        if (item.jenis === "Sakit") {
           dashboardData.jumlahSakit++;
-        } else if (item.jenis === 'Izin') {
+        } else if (item.jenis === "Izin") {
           dashboardData.jumlahIzin++;
         }
       });
+
+      // Jika ada data Semester yang cocok
+      if (semesterData.length > 0) {
+        dashboardData.tahun_akademik = semesterData[0].nama_semester;
+      }
+
       res.send(dashboardData);
     })
     .catch((err) => {
       res.status(500).send({
-        message: err.message || 'Some error occurred while retrieving Kelas.',
+        message: err.message || "Some error occurred while retrieving data.",
       });
     });
 };
