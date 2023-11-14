abb39bb3624ab19d8657d149d7970b93f34aae4d
adrianeka <78060253+adrianeka@users.noreply.github.com>
2023-11-14 20:05:47 +0700

    feat : dashboard admin

--- app/controllers/dashboard_admin_controller.js
+++ app/controllers/dashboard_admin_controller.js
@@ -2,7 +2,7 @@ const db = require("../models");
 const Perizinan = db.perizinan;
 const Semester = db.semester;
 
-exports.findAll = (req, res) => {
+exports.findOne = (req, res) => {
   const userId = req.userId;
 
   // Membuat Promise untuk mengambil data Perizinan
@@ -38,6 +38,9 @@ exports.findAll = (req, res) => {
       };
 
       perizinanData.forEach((item) => {
+        // Mendapatkan bulan dari createdAt
+        const createdAt = new Date(item.createdAt);
+        const month = createdAt.toLocaleString("default", { month: "long" });
         if (item.jenis === "Sakit") {
           dashboardData.jumlahSakit++;
         } else if (item.jenis === "Izin") {
@@ -55,6 +58,78 @@ exports.findAll = (req, res) => {
     .catch((err) => {
       res.status(500).send({
         message: err.message || "Some error occurred while retrieving data.",
+      });
+    });
+};
+
+// exports.findAll = (req, res) => {
+//   const userId = req.userId;
+//   Perizinan.findAll({
+//     where: {
+//       nim: userId,
+//       status: {
+//         [Op.not]: "Draft",
+//       },
+//     },
+//   })
+//     .then((data) => {
+//       res.send(data);
+//     })
+//     .catch((err) => {
+//       res.status(500).send({
+//         message: err.message || "Some error occurred while retrieving Kelas.",
+//       });
+//     });
+// };
+
+exports.findAll = (req, res) => {
+  const userId = req.userId;
+  const walidosen_id = req.params.walidosen_id;
+  Perizinan.findAll({
+    // where: {
+
+    // },
+    include: [
+      {
+        model: Semester,
+        where: {
+          status_semester: 1,
+        },
+      },
+      {
+        model: db.mahasiswa,
+        // where: {
+        //   walidosen_id: walidosen_id,
+        // },
+      },
+    ],
+  })
+    .then((data) => {
+      const dashboardData = {
+        jumlahSakit: 0,
+        jumlahIzin: 0,
+        totalPermohonan: 0,
+        jumlah_sakit_perbulan: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        jumlah_izin_perbulan: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      };
+      data.forEach((item) => {
+        const month = item.createdAt.getMonth();
+        if (item.jenis === "Sakit" && item.status === "Diverifikasi") {
+          dashboardData.jumlahSakit++;
+          dashboardData.jumlah_sakit_perbulan[month]++;
+        } else if (item.jenis === "Izin" && item.status === "Diverifikasi") {
+          dashboardData.jumlahIzin++;
+          dashboardData.jumlah_izin_perbulan[month]++;
+        }
+        if (item.status === "Menunggu Verifikasi") {
+          dashboardData.totalPermohonan++;
+        }
+      });
+      res.send(dashboardData);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving Kelas.",
       });
     });
 };
